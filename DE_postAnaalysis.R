33b805d5de4505f99b6ea48f234488c0e4b4ce38
silviapineda <silviapinedasanjuan@gmail.com>
2019-03-14 17:06:00 +0100

    Circos plot

--- DE_postAnaalysis.R
+++ DE_postAnaalysis.R
@@ -34,6 +34,10 @@ id<-match(rownames(norm_data_rlog),annotation$Ensemble_id)
 norm_data_rlog<-norm_data_rlog[which(is.na(id)==F),] #26526
 
 results_multi<-read.csv("Results/RNAseq/results_AMR_CMR_STA_ENET.csv")
+grep("2-Sep",results_multi$name) #20
+results_multi$name<-as.character(results_multi$name)
+results_multi$name[20]<-"SEPT2"
+
 results_AMR_STA<-read.csv("Results/RNAseq/results_STA_AMR_ENET.csv")
 results_AMR_CMR<-read.csv("Results/RNAseq/results_CMR_AMR_ENET.csv")
 results_REJ_STA<-read.csv("Results/RNAseq/results_REJ_STA_ENET.csv")
@@ -289,6 +293,7 @@ clin_vars<-clinData[,c(3,5:17,41)]
 colnames(clin_vars)<-c("DSA","ag","ai","at","ti","ptc","av","ah","cg","ci","ct","cv","cm","C4d","Time")
 rownames(clin_vars)<-clinData$Individual_id
 
+clin_vars<-clin_vars[,c("DSA","ag","ptc","C4d","cg","cv")]
 ####################################################
 ##  Correlation matrix between clinical variables ##
 
@@ -297,11 +302,11 @@ p.mat <- cor.mtest(na.omit(clin_vars))$p
 ord <- corrMatOrder(M, order = "AOE")
 M2 <- M[ord,ord]
 p.mat2 <-p.mat[ord,ord]
-tiff("Results/RNAseq/Corrplot_clinVars.tiff", res=300,w=2000,h=2000)
+tiff("Results/RNAseq/Corrplot_clinVars_AMR.tiff", res=300,w=2000,h=2000)
 corrplot.mixed(M2, lower.col = brewer.pal(n = 8, name = "RdBu"), upper.col = brewer.pal(n = 8, name = "RdBu"))
 dev.off()
 
-tiff("Results/RNAseq/Corrplot_clinVars.tiff", res=300,w=2000,h=2000)
+tiff("Results/RNAseq/Corrplot_clinVars_AMR.tiff", res=300,w=2000,h=2000)
 col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
 corrplot(M2, method = "color", col = col(200),
          type = "upper", order = "hclust", number.cex = .7,
@@ -354,15 +359,15 @@ names(lm_network_c)<-rownames(norm_data_rlog_results)
 edges<- data.frame(
   Genes = rep(names(lm_network_p), lapply(lm_network_p, length)), Clin = unlist(lapply(lm_network_p, names)),
   pvalue = abs(log(unlist(lm_network_p),10)), coef = unlist(lm_network_c))
-edges$color<-ifelse(edges$coef<0,"darkgoldenrod3","gray56")
+edges$color<-ifelse(edges$coef<0,"gray56","darkgoldenrod3")
 
 #Unique Genes with edges
 Genes<-as.character(unique(edges$Genes))
 #All genes significant in the DE analysis
 #Genes<-as.character(results$name)
-id_AMR<-na.omit(match(results_multi$name[which(results_multi$cluster==1)],Genes))
-id_CMR<-na.omit(match(results_multi$name[which(results_multi$cluster==3)],Genes))
-id_STA<-na.omit(match(results_multi$name[which(results_multi$cluster==2)],Genes))
+id_AMR<-na.omit(match(results_multi$name[which(results_multi$cluster=="AMR")],Genes))
+id_CMR<-na.omit(match(results_multi$name[which(results_multi$cluster=="TCMR")],Genes))
+id_STA<-na.omit(match(results_multi$name[which(results_multi$cluster=="STA")],Genes))
 
 nodes<-c(Genes[id_AMR],Genes[id_CMR],Genes[id_STA],colnames(clin_vars))
 COLOR = brewer.pal(4,"Pastel1")
@@ -383,7 +388,7 @@ plot(net,vertex.label.color="black",vertex.label.cex=1.3,layout=l*1,rescale=F)
 legend("topleft", c("AMR","TCMR", "STA","Clin"), pch=20,
        col=COLOR[1:4],  pt.cex=5,cex=1.5, bty="n", ncol=1)
 legend("bottomleft", c("Neg-association","Pos-associatiom"), lty=1,lwd=5,
-       col=c("darkgoldenrod3","gray56"), cex=1.5, bty="n", ncol=1)
+       col=c("gray56","darkgoldenrod3"), cex=1.5, bty="n", ncol=1)
 dev.off()
 
 
@@ -422,11 +427,11 @@ for (i in 1:ncol(norm_data_rlog_noncoding)){
 }
 pvalue_adj<-p.adjust(pvalue,method = "fdr")  
 maxp<-max(pvalue_adj[which(pvalue_adj<0.05)])
-grep(maxp, pvalue_adj) #position 690
+grep(maxp, pvalue_adj) #position 310
 pvalue_array<-as.numeric(pvalue)
-fdr_value<-pvalue_array[690] #pvalue=0.01070372 ##This is the FDR correction < 0.05
+fdr_value<-pvalue_array[310] #pvalue=0.01072245 ##This is the FDR correction < 0.05
 
-##2. Build the matrix
+##2. Build the dataframe
 lm_pvalue<-rep(NA,ncol(norm_data_rlog_coding))
 lm_coef<-rep(NA,ncol(norm_data_rlog_coding))
 lm_noncoding_p<-list()
@@ -448,7 +453,7 @@ names(lm_noncoding_c)<-colnames(norm_data_rlog_noncoding)
 
 noncoding_df<- data.frame(
   Genes_noncod = rep(names(lm_noncoding_p), lapply(lm_noncoding_p, length)), Genes_cod = unlist(lapply(lm_noncoding_p, names)),
-  pvalue = unlist(lm_noncoding_p), coef = unlist(lm_noncoding_p))
+  pvalue = unlist(lm_noncoding_p), coef = unlist(lm_noncoding_c))
 
 join_non_coding<-inner_join(noncoding_df,results_multi,by=c("Genes_noncod"="name"))
 join_non_coding<-join_non_coding[,c(1:4,9,13)]
@@ -456,8 +461,84 @@ colnames(join_non_coding)[5:6]<-c("type_noncod","cluster_noncod")
 join_coding<-inner_join(join_non_coding,results_multi,by=c("Genes_cod"="name"))
 join_coding<-join_coding[,c(1:6,15)]
 colnames(join_coding)[7]<-c("cluster_cod")
-noncoding_df<-join_coding
-write.csv(noncoding_df,"Results/RNAseq/results_noncoding.csv")
+noncoding_df<-data.frame(join_coding) #529
+noncoding_df$Genes_noncod<-factor(noncoding_df$Genes_noncod)
+noncoding_df$Genes_cod<-factor(noncoding_df$Genes_cod)
+
+##Order by effect size
+effect1 <- noncoding_df %>%
+  group_by(Genes_noncod) %>%
+  summarize(effect1 = sum(abs(coef)))
+effect2 <- noncoding_df %>%
+  group_by(Genes_cod) %>%
+  summarize(effect2 = sum(abs(coef)))
+
+noncoding_df$effect1<-data.frame(effect1[noncoding_df$Genes_noncod,2])
+noncoding_df$effect2<-data.frame(effect2[noncoding_df$Genes_cod,2])
+noncoding_df = noncoding_df[order(noncoding_df$effect1,noncoding_df$effect2,decreasing = T),]
+noncoding_df<-noncoding_df[,-c(8:9)]
+
+write.csv(noncoding_df,"Results/RNAseq/results_noncoding.csv",eol="\r")
+
+#########
+### 3. Circos plot with the relationships
+#########
+library(circlize)
+library(randomcoloR)
+
+df = data.frame(from = noncoding_df$Genes_noncod, to = noncoding_df$Genes_cod,value=noncoding_df$coef)
+effect1 <- df %>%
+  group_by(from) %>%
+  summarize(effect1 = sum(abs(value)))
+effect2 <- df %>%
+  group_by(to) %>%
+  summarize(effect2 = sum(abs(value)))
+
+df$effect1<-data.frame(effect1[df$from,2])
+df$effect2<-data.frame(effect2[df$to,2])
+df = df[order(df$effect1,decreasing = T),]
+df = df[order(df$effect2,decreasing = T),]
+
+df<-df[,-c(4:5)]
+
+##Colors
+#set.seed(54)
+#n <- length(unique(noncoding_df$Genes_noncod))
+#palette <- distinctColorPalette(n)
+
+COLOR = brewer.pal(4,"Pastel1")
+noncoding_df$color_noncod<-ifelse(noncoding_df$cluster_noncod=="AMR", COLOR [1],
+                                    ifelse(noncoding_df$cluster_noncod=="TCMR", COLOR[2], COLOR[3]))
+noncoding_df$color_cod<-ifelse(noncoding_df$cluster_cod=="AMR", COLOR [1],
+                                  ifelse(noncoding_df$cluster_cod=="TCMR", COLOR[2], COLOR[3]))
+
+noncoding_df_unique<-noncoding_df[!duplicated(noncoding_df$Genes_noncod),]
+coding_df_unique<-noncoding_df[!duplicated(noncoding_df$Genes_cod),]
+grid.col<-c(noncoding_df_unique$color_noncod,coding_df_unique$color_cod)
+names(grid.col)<-c(as.character(noncoding_df_unique$Genes_noncod),as.character(coding_df_unique$Genes_cod))
+
+circos.clear()
+tiff("Results/RNAseq/circos_coding_noncoding.tiff",res=300,h=3000,w=3000)
+chordDiagram(df,grid.col = grid.col,directional = -1, annotationTrack = "grid",preAllocateTracks = 1)
+             #link.sort = T, link.decreasing = T)
+
+# we go back to the first track and customize sector labels
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim), ylim[1], sector.name, facing = "clockwise",
+              niceFacing = TRUE, adj = c(0, 0.5),cex = 0.6)
+}, bg.border = NA) 
+dev.off()
+
+##Enrichment in non-coding
+counts=matrix(data=c(374,155,1012,916),nrow=2)
+chisq.test(counts) #p-value = 1.1*10-13
+
+##Enrichment in coding
+counts=matrix(data=c(324,139,924,1070),nrow=2)
+chisq.test(counts) #p-value = 7.923365e-20
 
 
 ###After looking for the GO biological terms in EnrichR
@@ -466,17 +547,6 @@ table(noncoding_df_GO$GO.Biological.Process)
 
 GO_terms <- group_by(noncoding_df_GO,GO.Biological.Process)
 summarize(GO_terms)
-
-##Build a co-expression matrix
-cordist <- function(dat) {
-  cor_matrix  <- cor(t(dat))
-  
-  dist_matrix <- as.matrix(dist(dat, diag=TRUE, upper=TRUE))
-  dist_matrix <- log1p(dist_matrix)
-  dist_matrix <- 1 - (dist_matrix / max(dist_matrix))
-  
-  sign(cor_matrix) * ((abs(cor_matrix) + dist_matrix)/ 2)
-}
 
 ###Find the significant genes in the normalize data
 coding_genes<-unique(as.character(noncoding_df$Genes_cod))
@@ -488,32 +558,7 @@ norm_data_rlog_results<-norm_data_rlog[match(annotation$Ensemble_id[id],rownames
 id_genes<-match(rownames(norm_data_rlog_results),annotation$Ensemble_id)
 rownames(norm_data_rlog_results)<-annotation$name[id_genes]
 
-###Build de similarity matrix
-sim_matrix <- cordist(norm_data_rlog_results)
-id_coding<-match(coding_genes,rownames(sim_matrix))
-id_noncoding<-match(non_coding_genes,rownames(sim_matrix))
-sim_matrix_2<-sim_matrix[id_coding,id_noncoding]
-  
-###Find which cluster is up-regulated
-results_multi$name<-as.character(results_multi$name)
-#results_multi$name[19]<-c("SEPT2")
-cluster_coding<-results_multi$cluster[match(rownames(sim_matrix_2),results_multi$name)]
-cluster_noncoding<-results_multi$cluster[match(colnames(sim_matrix_2),results_multi$name)]
-COLOR = brewer.pal(4,"Pastel1")
-ann_colors = list(cluster_coding = c("AMR" = COLOR [1] ,"TCMR" = COLOR[2], "STA" = COLOR[3]),
-                  cluster_noncoding  = c("AMR" = COLOR [1] ,"TCMR" = COLOR[2], "STA" = COLOR[3]))
-names(cluster_coding)<-rownames(sim_matrix_2)
-names(cluster_noncoding)<-colnames(sim_matrix_2)
 
-anno_row<-as.data.frame(ifelse(cluster_coding==1,"AMR",ifelse(cluster_coding==2,"TCMR","STA")))
-colnames(anno_row)<-"cluster_coding"
-anno_col<-as.data.frame(ifelse(cluster_noncoding==1,"AMR",ifelse(cluster_noncoding==2,"TCMR","STA")))
-colnames(anno_col)<-"cluster_noncoding"
-
-tiff("Results/RNAseq/SimilarityMatrix.tiff",res=300,h=3000,w=4000)
-pheatmap(sim_matrix_2,border_color=F,annotation_colors = ann_colors,
-         annotation_row = anno_row,annotation_col=anno_col)
-dev.off()
 
 
 ################################
